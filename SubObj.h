40a314670485388461d80d5d9c1ce70e5a698891
Roman Dzięcioł <roman.dzieciol@gmail.com>
2014-08-12 17:42:17 +0200

    init

---
+++ SubObj.h
@@ -0,0 +1,13 @@
+//
+//  SubObj.h
+//  xcbots_main
+//
+//  Created by HighTail on 12/08/14.
+//  Copyright (c) 2014 HighTail. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubObj : NSObject
+
+@end
---
+++ SubObj.m
@@ -0,0 +1,13 @@
+//
+//  SubObj.m
+//  xcbots_main
+//
+//  Created by HighTail on 12/08/14.
+//  Copyright (c) 2014 HighTail. All rights reserved.
+//
+
+#import "SubObj.h"
+
+@implementation SubObj
+
+@end
